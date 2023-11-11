8730f2a7bc4f530fa0e5a6a09c828294a6614976
AZRilso <53212931+chimmykk@users.noreply.github.com>
2023-11-11 23:18:43 +0530

    Update and rename scrap.js to scrap.ts

--- pages/api/scrap.ts
+++ pages/api/scrap.ts
@@ -1,44 +1,44 @@
-
-// pages/api/twitter-follower-count.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import { chromium } from 'playwright';
 
 async function scrapeTwitterFollowerCount() {
-  const browser = await chromium.launch({
-    headless: true,
-  });
-
-  const context = await browser.newContext({
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-  });
-
-  const page = await context.newPage();
-
   try {
+    const { chromium } = require('playwright');
+
+    const browser = await chromium.launch({
+      headless: true,
+    });
+
+    const context = await browser.newContext({
+      userAgent:
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    });
+
+    const page = await context.newPage();
+
     await page.setViewportSize({ width: 1366, height: 768 });
 
-    await page.goto('https://livecounts.io/embed/twitter-live-follower-counter/Thotsem22', { waitUntil: 'domcontentloaded', timeout: 0 });
+    await page.goto('https://livecounts.io/embed/twitter-live-follower-counter/Thotsem22', {
+      waitUntil: 'domcontentloaded',
+      timeout: 0,
+    });
 
     await page.waitForTimeout(5000);
 
     const followerCount = await page.evaluate(() => {
       const followerCountElements = Array.from(document.querySelectorAll('.odometer-value')).slice(0, 9);
-      return followerCountElements.map(element => element.textContent).join('');
+      return followerCountElements.map((element) => element.textContent).join('');
     });
 
     console.log(`Twitter Follower Count: ${followerCount}`);
 
     return followerCount;
   } catch (error) {
-    console.error('Error:', error);
-    return null;
-  } finally {
-    // Close the browser
-    await browser.close();
+    console.error('Error during scraping:', error);
+    throw error; // Rethrow the error to be caught in the calling function
   }
 }
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const followerCount = await scrapeTwitterFollowerCount();
 
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error in handler:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
